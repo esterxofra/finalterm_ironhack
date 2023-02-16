cec4d14c788ef8a633310e287c0b3a7df76b979d
Ester Xofra <esterxofra@gmail.com>
2023-02-16 11:36:56 +0100

    styling

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -3,58 +3,59 @@
     <div class="image"></div>
 
     <div class="container">
-      <div>
-        <h1 class="header-title">Welcome to DONE</h1>
-        <p class="header-subtitle">
-          Start Organizing your daily goals with the tracker task!
-        </p>
-      </div>
-
-      <form @submit.prevent="signIn" class="form-sign-in">
-        <h2>Sign in to continue.</h2>
-
-        <div class="form">
-          <div class="form-input">
-            <label for="uname"><b>Email:</b></label>
-            <input
-              type="email"
-              name="uname"
-              class="input-field"
-              placeholder="example@gmail.com"
-              id="email"
-              v-model="email"
-              required
-            />
-          </div>
-
-          <div class="form-input">
-            <label for="psw"><b>Password:</b></label>
-            <input
-              type="password"
-              name="psw"
-              class="input-field"
-              placeholder="**********"
-              id="password"
-              v-model="password"
-              required
-            />
-          </div>
-
-          <button class="button" type="submit">Sign In</button>
-        </div>
-
+      <div class="internal-container">
         <div>
-          <p>
-            Don't have an account?
-            <PersonalRouter
-              :route="route"
-              :buttonText="buttonText"
-              class="sign-in-link"
-            />
+          <h1 class="header-title">Welcome back!</h1>
+          <p class="header-subtitle">
+            Start Organizing your daily goals with the tracker task!
           </p>
-          <p>It takes less than a minute.</p>
         </div>
-      </form>
+
+        <form @submit.prevent="signIn" class="form-sign-in">
+          <h2>Sign in to continue.</h2>
+
+          <div class="form">
+            <div class="form-input">
+              <label for="uname"><b>Email:</b></label>
+              <input
+                type="email"
+                name="uname"
+                class="input-field"
+                placeholder="example@gmail.com"
+                id="email"
+                v-model="email"
+                required
+              />
+            </div>
+
+            <div class="form-input">
+              <label for="psw"><b>Password:</b></label>
+              <input
+                type="password"
+                name="psw"
+                class="input-field"
+                placeholder="**********"
+                id="password"
+                v-model="password"
+                required
+              />
+            </div>
+
+            <button class="button" type="submit">Sign In</button>
+          </div>
+
+          <div>
+            <p>
+              Don't have an account?
+              <PersonalRouter
+                :route="route"
+                :buttonText="buttonText"
+                class="sign-in-link"
+              />
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
